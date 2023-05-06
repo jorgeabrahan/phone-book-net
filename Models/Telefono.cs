52725a9d3a711739d3fdd114dcf7ada8d470be8a
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-06 16:11:23 -0600

    Create foreign keys data annotations

--- Models/Telefono.cs
+++ Models/Telefono.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace agenda.Models;
 
 public class Telefono
@@ -6,8 +7,10 @@ public class Telefono
   [Key]
   public Guid TelefonoId { get; set; }
 
+  [ForeignKey("CodigoAreaId")]
   public Guid CodigoAreaId { get; set; }
 
+  [ForeignKey("PersonaId")]
   public Guid PersonaId { get; set; }
 
   public string Numero { get; set; } = "";
