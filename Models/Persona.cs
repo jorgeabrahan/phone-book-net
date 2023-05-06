ef3e6a9dbf8ae90de0fad05fd974fe5e51bfa7eb
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-06 16:10:22 -0600

    Add model data annotations

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -1,12 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace agenda.Models;
 
 public class Persona
 {
+  [Key]
   public Guid PersonaId { get; set; }
+
+  [Required]
+  [MaxLength(250)]
   public string? Nombre { get; set; }
+
+  [Required]
+  [MaxLength(250)]
   public string? Apellido { get; set; }
+
+  [MaxLength(100)]
   public string? Alias { get; set; }
   public DateOnly FechaNacimiento { get; set; }
+
+  [NotMapped]
+  public int Edad { get; set; }
+
   public Sexos Sexo { get; set; }
   public EstadosCiviles EstadoCivil { get; set; }
   public string? Correo { get; set; }
--- Models/Telefono.cs
+++ Models/Telefono.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
 namespace agenda.Models;
 
 public class Telefono
 {
+  [Key]
   public Guid TelefonoId { get; set; }
+
   public Guid CodigoAreaId { get; set; }
+
   public Guid PersonaId { get; set; }
+
   public string Numero { get; set; } = "";
 
   public virtual CodigoArea? CodigoArea { get; set; }
