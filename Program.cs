8adf85e9f5b4251a7a4ca277f048b8ff6a135959
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-13 18:25:54 -0600

    Setup sql server service and create root endpoint and 'connect' endpoint to validate connection

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using agenda.Models;
+using agenda;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSqlServer<PhoneBookContext>("Data Source=localhost;Initial Catalog=AgendaDB;user id=sa;password=C0ntrol1*;Encrypt=False");
 var app = builder.Build();
 
-app.MapGet("/", () => "Hola mundo!");
+app.MapGet("/", () => "Phone Book HomePage");
+// endpoint to validate connection!
+app.MapGet("/connect", ([FromServices] PhoneBookContext context) =>
+{
+  context.Database.EnsureCreated();
+  return Results.Ok("Base de datos en linea!");
+});
 
 app.Run();
